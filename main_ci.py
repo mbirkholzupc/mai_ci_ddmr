c7652362baae4d57e9c4cb7221fd7842688f55e9
Mike <michael.birkholz@estudiantat.upc.edu>
2020-12-04 12:17:58 +0100

    Some config file/preprocessing flow stuff

--- main_ci.py
+++ main_ci.py
@@ -1,8 +1,22 @@
-if __name__ == "__main__":
+from utils.config_file import config_file
+
+def main_ci():
     from preprocess import preprocess_main
     from train import train_main
     from test import test_main
 
-    preprocess_main()
+    print('Reading config file: \'config.txt\'')
+    cfg = config_file('config.txt')
+    print(cfg)
+
+    # Right now, config_file object passed into each step. We may want to consider different
+    # configs for each step, especially if we want to run preprocessing once and then
+    # train/test multiple times
+    pp_imgs = preprocess_main(cfg)
+    print('Preprocessed images are in: ' + pp_imgs)
     train_main()
     test_main()
+
+# Call it this way so we can call from external modules like jupyter notebook also
+if __name__ == "__main__":
+    main_ci()
