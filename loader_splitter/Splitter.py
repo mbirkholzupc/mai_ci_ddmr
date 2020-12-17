27ab0dc84a8aeb0c806a5968e76834c2ce06bb59
David Fernández Aldana <luskaner@gmail.com>
2020-12-17 14:59:54 +0100

    Fixed repeated images and shuffle the data

--- loader_splitter/Splitter.py
+++ loader_splitter/Splitter.py
@@ -2,20 +2,26 @@ from sklearn.model_selection import StratifiedKFold
 
 from loader_splitter.Split import Split
 from loader_splitter.SupervisedData import SupervisedData
-
+from sklearn.utils import shuffle
 
 class Splitter:
     """
     Allows to split the images in training and test sets
     """
     def __init__(self):
-        self._skf = StratifiedKFold(3)
+        self._skf = StratifiedKFold(3, shuffle=True)
 
     def split(self, supervised_data: SupervisedData):
         """
         Split the images in training and test sets
         """
         for train_index, test_index in self._skf.split(supervised_data.X, supervised_data.y):
-            train_set = SupervisedData(supervised_data.X[train_index], supervised_data.y[train_index])
-            test_set = SupervisedData(supervised_data.X[test_index], supervised_data.y[test_index])
+            X_train = supervised_data.X[train_index]
+            y_train = supervised_data.y[train_index]
+            X_test = supervised_data.X[test_index]
+            y_test = supervised_data.y[test_index]
+            shuffle(X_train, y_train, replace=True)
+            shuffle(X_test, y_test, replace=True)
+            train_set = SupervisedData(X_train, y_train)
+            test_set = SupervisedData(X_test, y_test)
             yield Split(train_set, test_set)
