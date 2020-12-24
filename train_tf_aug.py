2ff3f732b687521aa5670943e913f77ca7879be0
David Fernández Aldana <luskaner@gmail.com>
2020-12-24 14:40:37 +0100

    Added main for tf aug!

---
+++ train_tf_aug.py
@@ -0,0 +1,10 @@
+from models.pretrained.main_aug_tf import main
+
+
+def train_main():
+    main()
+
+
+if __name__ == "__main__":
+    # Process command-line args here (or maybe we prefer a config file)
+    train_main()
