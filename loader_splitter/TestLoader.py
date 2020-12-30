45924bfb01ed40f27707677f850c4a2a8c427a45
David Fernández Aldana <luskaner@gmail.com>
2020-12-30 20:12:22 +0100

    Added Test and Train loaders

---
+++ loader_splitter/TestLoader.py
@@ -0,0 +1,12 @@
+from loader_splitter.Loader import Loader
+from os.path import realpath, dirname, join
+
+
+class TestLoader(Loader):
+    """
+    Allows to loads the images with their labels
+    """
+
+    def __init__(self):
+        categories = ["benign", "malignant"]
+        super().__init__(join(dirname(realpath(__file__)), "./data/test/all"), categories)
