17205ff95416002cfba61de2e0a0e68501d3f933
Mike <michael.birkholz@estudiantat.upc.edu>
2021-01-04 09:33:51 +0100

    Revert to 50 epochs

--- models/ModelRunner.py
+++ models/ModelRunner.py
@@ -144,7 +144,7 @@ class ModelRunner():
                          **{"loss": "binary_crossentropy", "metrics": ["accuracy",
                                                                        tf.keras.metrics.Precision(name="precision"),
                                                                        tf.keras.metrics.Recall(name="recall")]}})
-        base_fit_params = {"epochs": 100, "validation_data": (validation_data.X, validation_data.y),
+        base_fit_params = {"epochs": 50, "validation_data": (validation_data.X, validation_data.y),
                            "callbacks": [es, learning_rate_reduction], "verbose": 2}
 
         if type(train_data) is SupervisedData:
