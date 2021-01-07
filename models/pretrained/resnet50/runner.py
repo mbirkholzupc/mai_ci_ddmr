94c3168943c0fbcd553999f3b126a3928f625371
David Fernández Aldana <luskaner@gmail.com>
2021-01-07 18:25:20 +0100

    Resnet now freezes certain layers does no pooling and instead does a flatten

--- models/pretrained/resnet50/runner.py
+++ models/pretrained/resnet50/runner.py
@@ -1,5 +1,6 @@
 from tensorflow.keras import Sequential, Input
 from tensorflow.keras.layers import Dense
+from tensorflow.python.layers.core import Flatten
 
 from models.BestAugmentedModelRunner import AugmentedModelRunner, BaseModelRunner
 from tensorflow.keras.applications import resnet50 as Resnet50
@@ -13,13 +14,15 @@ def model_builder(intermediate_layers):
         name="ResNet50"
     )
 
-    resnet = Resnet50.ResNet50(include_top=False, pooling='avg')
+    resnet = Resnet50.ResNet50(include_top=False)
 
     # Freeze resnet layers
+    # See https://radiant-brushlands-42789.herokuapp.com/towardsdatascience.com/deep-learning-using-transfer-learning-python-code-for-resnet50-8acdfb3a2d38
     for layer in resnet.layers:
-        layer.trainable = False
+        layer.trainable = layer.name in ['res5c_branch2b', 'res5c_branch2c', 'activation_97']
 
     model.add(resnet)
+    model.add(Flatten())
 
     for layer in intermediate_layers:
         model.add(layer)
