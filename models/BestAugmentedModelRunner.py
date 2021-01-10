e3edc645ae2f19672060dd006faeea36220123f2
David Fernández Aldana <luskaner@users.noreply.github.com>
2021-01-10 15:02:38 +0100

    Update BestAugmentedModelRunner.py

--- models/BestAugmentedModelRunner.py
+++ models/BestAugmentedModelRunner.py
@@ -5,7 +5,6 @@ class AugmentedModelRunner(BaseModelRunner):
     def __init__(self, model_builder, image_size=(224, 224), batch_size=128):
         super().__init__(model_builder, image_size, batch_size, self._get_augmentation(), True, True)
 
-    # TODO: Change the following params as the best obtained in MultipleAugmentedModelRunner
     @staticmethod
     def _get_augmentation():
         return {
