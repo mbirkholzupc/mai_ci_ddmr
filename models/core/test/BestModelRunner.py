82c52f378037f9d181882892a2b973ab18e0fea0
David Fernández Aldana <luskaner@gmail.com>
2021-01-10 17:31:54 +0100

    evaluate best model fix

--- models/core/test/BestModelRunner.py
+++ models/core/test/BestModelRunner.py
@@ -1,6 +1,6 @@
 from tensorflow.keras.optimizers import RMSprop
 
-from models.core.validation.ModelRunner import ModelRunner
+from models.core.test.ModelRunner import ModelRunner
 from models.scratch.runner import model_builder as scratch_model_builder
 from models.pretrained.inceptionv4.runner import model_builder as inceptionv4_model_builder
 from models.pretrained.resnet50.runner import model_builder as resnet50_model_builder
